3f5ed413a93f31aa69ab83979c20181a40e1e6d4
김현용 <multikhy0612@gmail.com>
2025-11-26 00:21:07 +0900

    V 1.0.0 루트시 html 설정

--- app.mjs
+++ app.mjs
@@ -18,10 +18,15 @@ app.use(express.urlencoded({ extended: true }));
 // 회원가입
 app.post("/auth/signup", AR.signup);
 
+// 루트 접속 시 login.html로 리다이렉트
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
 // 404 처리
-// app.use((req, res) => {
-//   res.status(404).json({ message: "Not Found" });
-// });
+app.use((req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
 
 app.listen(port, () => {
   console.log(`${port} 포트 서버 구동 중...`);
